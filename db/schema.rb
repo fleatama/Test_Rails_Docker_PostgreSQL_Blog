6d16d5b0f3cc75cceb1693049b692cc325071d1f
fleatama <fleatama@gmail.com>
2020-06-12 15:03:15 +0900

    destroy pages_controller and page_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_083834) do
+ActiveRecord::Schema.define(version: 2020_06_12_055047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.text "title"
