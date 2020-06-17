4e85c777a61462e6a7a6b1f34b2977f4803a1dd6
fleatama <fleatama@gmail.com>
2020-06-17 20:36:40 +0900

    create model article_category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_103654) do
+ActiveRecord::Schema.define(version: 2020_06_17_113105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
