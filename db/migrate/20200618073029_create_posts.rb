e69f302daea0b9fe6ccba60afad9ce0c2a287766
fleatama <fleatama@gmail.com>
2020-06-18 16:33:44 +0900

    create post and tag and post_tag

---
+++ db/migrate/20200618073029_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.integer :mode
+      t.string :desc
+
+      t.timestamps
+    end
+  end
+end
